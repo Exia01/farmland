35926ee57c1df8da212bdf8378112328fbfa827f
Jose <gonzalezj@protonmail.com>
2021-03-18 22:13:40 -0400

    Update package json. Add imports in server js file

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const { port, env } = require('./config/variables');
-const app = require('./config/express');
-const mongoose = require('./config/mongoose');
-
+const { port, env } = require('./server/config/variables');
+const app = require('./server/config/express');
+const mongoose = require('./server/config/mongoose');
+const logger = require('./server/config/logger');
+const testCall = require('./server/utils/testAxiosCalls');
 // open mongoose connection
 mongoose.connect();
 
 // listen to requests
-app.listen(port, () => console.log(`Server started on port ${port} ${env}`));
+// app.listen(port, () => console.log(`Server started on port ${port} ${env}`));
 
+// listen to requests
+
+app.listen(port, () => logger.info(`server started on port ${port} (${env})`));
 /**
  * Exports express
  * @public
  */
+
 module.exports = app;
+
+
+// No need for bluebird:https://masteringjs.io/tutorials/fundamentals/bluebird
