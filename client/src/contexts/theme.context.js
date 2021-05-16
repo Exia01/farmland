2427ab8c8473eb5c2d7c2b5054150b4f3134e665
Jose <gonzalezj@protonmail.com>
2021-05-16 15:22:08 -0400

    add theme context and constants file

---
+++ client/src/contexts/theme.context.js
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useReducer } from 'react';
+import { createMuiTheme } from '@material-ui/core';
+
+import { themeReducer, initializer } from '../reducer/themeReducer';
+
+export const ThemeContext = createContext();
+export const SetContext = createContext();
+
+export default function CurrentThemeProvider(props) {
+  const [themePreference, dispatch] = useReducer(themeReducer, {}, initializer);
+  const theme = createMuiTheme({
+    palette: {
+      type: themePreference.palette.type,
+    },
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      'localThemePreference',
+      JSON.stringify(themePreference)
+    );
+    console.log(themePreference);
+  }, [themePreference]);
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      {/* wrapping inside makes it so that we only have to wrap components with one provider */}
+      <SetContext.Provider value={dispatch}>
+        {props.children}
+      </SetContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
---
+++ client/src/reducer/themeReducer.js
@@ -0,0 +1,38 @@
+export const initializer = () => {
+  const initialState = {
+    palette: {
+      type: 'light',
+    },
+    expiresAt: {},
+  };
+
+  // const paletteType JSON.parse(localStorage.getItem('localTheme')) || initialState;
+  return (
+    JSON.parse(localStorage.getItem('localThemePreference')) || initialState
+  );
+};
+
+export const themeReducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      const updatedType = state.palette.type === 'light' ? 'dark' : 'light';
+      //   let updatedState = JSON.parse(JSON.stringify(state));
+      const updateThemePref = {
+        ...state,
+        palette: {
+          ...state.palette,
+          type: updatedType,
+        },
+      };
+      console.log(updateThemePref);
+      return updateThemePref;
+
+    default:
+      return state;
+  }
+};
+
+export const toggleTheme = (theme) => ({
+  type: 'TOGGLE_THEME',
+  theme,
+});
