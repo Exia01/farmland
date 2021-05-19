a976259c913473065a3c8bf80cff3ddc4be7aea8
Jose <gonzalezj@protonmail.com>
2021-05-18 22:36:40 -0400

    update hero-banner

--- client/src/components/hero-banner/hero-banner.component.jsx
+++ client/src/components/hero-banner/hero-banner.component.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
-
 // css
 import classes from './hero-banner.module.css';
+import heroBackgroundImage from '../../uploads/images/banners/michael-bourgault-YvvHEQNgMcU-unsplash-v2.jpg';
 
 // jss
 // import useStyles from './hero-banner.styles';
 
+// Material Ui
+import { Container, Grid, Paper } from '@material-ui/core';
+
 export default function HeroBanner() {
   // const classes = useStyles
   return (
-    <div className={classes.root}>
+    <div
+      className={classes.root}
+      style={{ backgroundImage: `url(${heroBackgroundImage})` }}
+    >
       <div className={classes.backgroundImage}></div>
-      <div className={classes.titleContainer}>
-        <p classes={classes.heroImage}>
-          <img src='' alt='' srcset='' />
-        </p>
-        <p classes={classes.title}>FARMLAND</p>
-        <p classes={classes.btnTitle}>
-          Shop now <span>Icon</span>
-        </p>
-      </div>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper} elevation={0}>
+              <div className={classes.titleContainer}>
+                <p classes={classes.heroImage}>
+                  <img src='' alt='' srcset='' />
+                </p>
+                <p classes={classes.title}>FARMLAND</p>
+                <p classes={classes.btnTitle}>
+                  Shop now <span>Icon</span>
+                </p>
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
