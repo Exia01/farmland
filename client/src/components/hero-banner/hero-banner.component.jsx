16a40b8180b92ceb2a585532a663739c8c7504cd
Jose <gonzalezj@protonmail.com>
2021-05-20 21:52:51 -0400

    update hero-banner

--- client/src/components/hero-banner/hero-banner.component.jsx
+++ client/src/components/hero-banner/hero-banner.component.jsx
@@ -1,38 +1,61 @@
-// css
-import classes from './hero-banner.module.css';
-import heroBackgroundImage from '../../uploads/images/banners/michael-bourgault-YvvHEQNgMcU-unsplash-v2.jpg';
+import { Link } from 'react-router-dom';
 
+// css
+import styles from './hero-banner.module.css';
+import heroBackgroundImage from '../../uploads/images/banners/michael-bourgault-YvvHEQNgMcU-unsplash-v2.jpg';
+import logo from '../../uploads/images/icons/farmLogo.png';
 // jss
 // import useStyles from './hero-banner.styles';
 
 // Material Ui
 import { Container, Grid, Paper } from '@material-ui/core';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 export default function HeroBanner() {
   // const classes = useStyles
   return (
     <div
-      className={classes.root}
+      className={styles.root}
       style={{ backgroundImage: `url(${heroBackgroundImage})` }}
     >
-      <div className={classes.backgroundImage}></div>
-      <Container>
+      <div className={styles.overlay}>
+        <div className={styles.contentContainer}>
+          <div className={styles.imgDiv}>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className={styles.item}>
+            <Link to='/shop'>
+              <p className={styles.title}>Shop Now</p>
+              <p>
+                {' '}
+                <ShoppingBasketIcon />
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <Container className={classes.indexSet}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Paper className={classes.paper} elevation={0}>
+            <Paper className={classes.paperDiv} elevation={0}>
               <div className={classes.titleContainer}>
-                <p classes={classes.heroImage}>
-                  <img src='' alt='' srcset='' />
-                </p>
-                <p classes={classes.title}>FARMLAND</p>
-                <p classes={classes.btnTitle}>
-                  Shop now <span>Icon</span>
-                </p>
+                <div className={classes.logo}>
+                  <img src={logo} alt='logo' />
+                </div>
+                <div className={classes.btnBox}>
+                  <p classes={classes.title}>FARMLAND</p>
+                  <p classes={classes.titleIcon}>
+                    Shop now <span>Icon</span>
+                  </p>
+                </div>
               </div>
             </Paper>
           </Grid>
         </Grid>
-      </Container>
-    </div>
-  );
+      </Container> */
 }
