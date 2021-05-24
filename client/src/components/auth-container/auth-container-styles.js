f8cc497d9c3f398c4b0cf5de5785e5d07a448e81
Jose <gonzalezj@protonmail.com>
2021-05-24 10:05:02 -0400

    Update auth-container-styles.js

--- client/src/components/auth-container/auth-container-styles.js
+++ client/src/components/auth-container/auth-container-styles.js
@@ -10,11 +10,16 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   navMenu: {
+    display: 'none',
     borderRadius: '50%',
     transition: 'fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
     padding: '12px',
     '&:hover': {
       backgroundColor: 'rgba(255, 255, 255, 0.08)',
+    },
+    // marginRight: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
     },
   },
 }));
