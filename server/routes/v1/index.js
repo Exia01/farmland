8d415f905888abbbe6e52864fbf237650dc1deb1
Jose <gonzalezj@protonmail.com>
2021-05-29 09:58:41 -0400

    update routes index

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const productRoutes = require('./product.route');
 const productVariantRoutes = require('./productVariant.route');
+const userRoutes = require('./user.route');
 
 
-const  messageRoutes  = require('../../utils/messages/messages.router');
+const messageRoutes = require('../../utils/messages/messages.router');
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.get('/status', (req, res) => res.status(200).send('OK'));
 router.use('/products', productRoutes);
 router.use('/products/:product_id/variants', productVariantRoutes);
 router.use('/variants', productVariantRoutes);
+router.use('/user', userRoutes);
 
 /**
  * GET messages for auth test
