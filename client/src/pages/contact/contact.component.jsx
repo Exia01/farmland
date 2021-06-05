06de13164aecc57ffade5444f953ef728ad6d4c9
ehoversten <ehoversten@gmail.com>
2021-06-05 14:27:20 -0400

    contact form submittion functionality working

--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Grid, makeStyles, Paper, TextField, Typography } from '@material-ui/core';
+import React, { useState, useContext } from 'react';
+import { Container, Grid, makeStyles, Paper, TextField, Typography, Button } from '@material-ui/core';
 // import useStyles from '../../components/header/header.styles';
 import styles from './contact.module.css';
 import logo from '../../uploads/images/icons/farmLogo.png';
@@ -18,6 +18,27 @@ const useStyles = makeStyles({
 
 const Contact = () => {
     const classes = useStyles();
+
+    const [contactName, setContactName] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
+    const [contactMessage, setContactMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let commentSubmission = {
+            contact_name: contactName,
+            contact_email: contactEmail,
+            message: contactMessage
+        }
+
+        console.log(commentSubmission);
+
+        // Reset Fields
+        setContactName('')
+        setContactEmail('')
+        setContactMessage('')
+    }
 
     return (
         <div>
@@ -57,9 +78,17 @@ const Contact = () => {
                     </Typography>
                 </Grid>
                 <Grid item xs='12' container spacing={3} justify='center'>
-                    <form noValidate autoComplete='off' className={classes.form_container}>
+                    <form
+                        onSubmit={handleSubmit} 
+                        noValidate 
+                        autoComplete='off' 
+                        className={classes.form_container}
+                        >
                         <Grid item xs='12' md='6'>
                             <TextField 
+                                onChange={(e) => setContactName(e.target.value) }
+                                value={contactName}
+                                id="outlined-basic"
                                 label='Name'
                                 className={classes.field}
                                 variant='outlined'
@@ -67,7 +96,10 @@ const Contact = () => {
                             </TextField>
                         </Grid>
                         <Grid item xs='12' md='6'>
-                            <TextField 
+                            <TextField
+                                onChange={(e) => setContactEmail(e.target.value) }
+                                value={contactEmail}
+                                id="outlined-basic"
                                 label='Email'
                                 className={classes.field}
                                 variant='outlined'
@@ -75,7 +107,9 @@ const Contact = () => {
                             </TextField>
                         </Grid>
                         <Grid item xs='12'>
-                            <TextField 
+                            <TextField
+                                onChange={(e) => setContactMessage(e.target.value) } 
+                                value={contactMessage}
                                 id='outlined-multiline-flexible'
                                 label='Message'
                                 className={classes.field}
@@ -84,6 +118,9 @@ const Contact = () => {
                                 multiline
                                 rows={4}>
                             </TextField>
+                        </Grid>
+                        <Grid item xs='12'>
+                            <Button variant='contained' color='primary' type='submit'>Submit</Button>
                         </Grid>
                     </form>
                 </Grid>
