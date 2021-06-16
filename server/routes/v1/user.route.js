74882fca0abaf7761be5398022c7d664ba7e298e
Jose <gonzalezj@protonmail.com>
2021-06-16 13:38:52 -0400

    Update user.route.js

--- server/routes/v1/user.route.js
+++ server/routes/v1/user.route.js
@@ -4,10 +4,7 @@ const userController = require('../../controllers/user.controller');
 // Routes start with /auth
 // router.get('/', userController.index);
 router.post('/', userController.createUser);
-router.post('/login', userController.loginUser);//could also name /authenticate
-
-// router.get('/:product_id', userController.getProduct);
-// router.put('/:product_id', userController.updateProduct);
-// router.delete('/:product_id', userController.deleteProduct);
+router.post('/login', userController.loginUser); //could also name /authenticate
+router.get('/verify-token', userController.verifyToken); //could also implement middleware?
 
 module.exports = router;
