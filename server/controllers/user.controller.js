d28c53164b4c82e56b1a29915ec69fe0f5a7826f
Jose <gonzalezj@protonmail.com>
2021-06-16 14:39:18 -0400

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const jwtDecode = require('jwt-decode');
 const {
@@ -33,7 +34,6 @@ const User = require('../models/user.model');
 // @@
 exports.createUser = async (req, res, next) => {
   try {
-    console.log(req.body);
     let { name, email, password, passwordCheck } = req.body;
 
     if (!email || !password) {
@@ -81,7 +81,8 @@ exports.createUser = async (req, res, next) => {
         role,
       };
 
-      // res.cookie('token', token, { httpOnly: true }).send();
+      //tell express to include cookie
+      res.cookie('token', token, { httpOnly: true });
       // Send JSON response
       return res.status(200).json({
         msg: 'User created!',
@@ -127,14 +128,13 @@ exports.loginUser = async (req, res, next) => {
       const { password, name, ...rest } = currentUser;
       //Copying onto new obj with the rest of properties
       const userInfo = Object.assign({}, { ...rest });
-      console.log(userInfo);
       const token = createToken(userInfo);
 
       const decodedToken = jwtDecode(token);
       const expiresAt = decodedToken.exp;
 
       // send token in HTTP-only Cookie
-      // res.cookie('token', token, { httpOnly: true }).send();
+      res.cookie('token', token, { httpOnly: true });
 
       // Send Successful Response
       res.status(200).json({
@@ -151,64 +151,36 @@ exports.loginUser = async (req, res, next) => {
   }
 };
 
-// // @@ LOGOUT ROUTE
-// // @@
-// router.get('/logout', (req, res) => {
-//   // res.headers({ "x-auth-token": "" });
-//   res.cookie('token', '', { httpOnly: true, expires: new Date(0) }).send();
-// });
 
 // // @@ VERIFICATION ROUTE
 // // @@
-// router.get('/verify-token', async (req, res) => {
-//   try {
-//     // Check Cookie for Token
-//     const cookie = req.cookies.token;
-//     if (!cookie) {
-//       return res.json(false);
-//     }
-//     // -- Check Header for Token
-//     // const token = req.header("x-auth-token");
-//     // if(!token) return res.json(false);
 
-//     // -- Verify Token
-//     // const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-//     const verified = jwt.verify(cookie, process.env.TOKEN_SECRET);
+exports.verifyToken = async (req, res, next) => {
+  try {
+    // Check Cookie for Token
+    const cookie = req.cookies.token;
+    if (!cookie) {
+      return res.json(false);
+    }
 
-//     if (!verified) return res.json(false);
+    // -- Verify Token
+    const verified = jwt.verify(cookie, process.env.TOKEN_SECRET);
+    console.log('verified', verified);
 
-//     // -- Valid User (?)
-//     const user = await User.findById({ _id: verified.id });
-//     // const user = await User.findById({ _id: verified.user });
-//     // console.log(user);
-//     if (!user) return res.json(false);
+    if (!verified) return res.json(false);
 
-//     // // Set User ID
-//     // req.user = user;
-//     // console.log(req.user);
+    // -- Valid User (?)
+    const user = await User.findById({ _id: verified.user._id })
+      .lean()
+      .exec();
 
-//     // -- Return TRUE if valid token || to WHERE ??
-//     return res.json(true);
-//   } catch (err) {
-//     console.log(err);
-//     return res.status(400).json(false);
-//   }
+    console.log(user);
+    if (!user) return res.json(false);
 
-//   // Set User ID
-//   // req.user = verified.user;
-//   // console.log(req.user);
-//   // // Call Next middleware
-//   // console.log('Calling NEXT Middleware');
-//   // next();
-// });
-
-// // -- TESTING -- //
-// router.get('/admin', async (req, res) => {
-//   try {
-//     // let users = await User.find({});
-//     let users = await User.find({}).populate('user_favorites');
-//     res.status(200).json(users);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+    // -- Return TRUE if valid token || to WHERE ??
+    return res.json(true);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(false);
+  }
+};
