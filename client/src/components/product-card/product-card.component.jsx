96ee8f770159cf26df0ef78f53780af4681bd45a
Jose <gonzalezj@protonmail.com>
2021-06-28 15:15:55 -0400

    Add margin, remove extra div. Format product page

--- client/src/components/product-card/product-card.component.jsx
+++ client/src/components/product-card/product-card.component.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Button, Typography, makeStyles } from '@material-ui/core'
+import { Button, Typography, makeStyles } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,43 +9,48 @@ import CardMedia from '@material-ui/core/CardMedia';
 import temp_product_img from '../../uploads/images/fruits/blueberries/joanna-kosinska-4qujjbj3srs-unsplash.jpg';
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: 345,
-    },
-    media: {
-        height: 250,
-    },
-})
+  root: {
+    maxWidth: 345,
+    margin: '0 auto',
+  },
+  media: {
+    height: 250,
+  },
+});
 
 const ProductCard = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <Card className={classes.root}>
-                <CardActionArea>
-                    <CardMedia
-                    className={classes.media}
-                    image={temp_product_img}
-                    title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Product Title
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, adipisci nisi? Quae sit, et expedita libero facilis corporis, nostrum nesciunt porro id iure dicta vitae ut quasi mollitia, officia ipsam!
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary" href="/products/5">
-                    Learn More
-                    </Button>
-                </CardActions>
-            </Card>
-        </div>
-    )
-}
+  return (
+    <>
+      {' '}
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={temp_product_img}
+            title='Contemplative Reptile'
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+              Product Title
+            </Typography>
+            <Typography variant='body2' color='textSecondary' component='p'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Distinctio, adipisci nisi? Quae sit, et expedita libero facilis
+              corporis, nostrum nesciunt porro id iure dicta vitae ut quasi
+              mollitia, officia ipsam!
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size='small' color='primary' href='/products/5'>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
 
 export default ProductCard;
