b8ae88b0e3eae6523439c42312a411f226d6c558
Jose <gonzalezj@protonmail.com>
2021-07-04 22:22:41 -0400

    Update logout-button.component.jsx
    This accounts for the account component

--- client/src/components/logout-button/logout-button.component.jsx
+++ client/src/components/logout-button/logout-button.component.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { AuthDispatchContext } from '../../contexts/auth.context';
 // Material UI
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 
 const LogoutButton = (props) => {
   const dispatch = useContext(AuthDispatchContext);
@@ -9,11 +9,22 @@ const LogoutButton = (props) => {
   const onClickHandler = (e) => {
     dispatch({ type: 'LOGOUT' });
   };
-  return (
+
+  let btnTag = (
     <Typography variant='button' onClick={onClickHandler}>
       Log Out
     </Typography>
   );
+
+  if (props.accountBtn) {
+    btnTag = (
+      <Button variant='outlined' onClick={onClickHandler}>
+        Log Out
+      </Button>
+    );
+  }
+
+  return btnTag;
 };
 
 export default LogoutButton;
