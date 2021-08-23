53f1bb9940d20f43bef60b98ad64f79decf4e63a
Jose <gonzalezj@protonmail.com>
2021-08-23 17:53:10 -0400

    Update homepage.component.jsx

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,37 +1,22 @@
 import { Container, Grid, Paper } from '@material-ui/core';
 import HeroBanner from '../../components/hero-banner/hero-banner.component';
+import SegmentTitle from './../../components/segment-title/segment-title.component';
+import SegmentModule from './../../components/segment-module/segment-module.component';
 
+// Styles
 import useStyles from './homepage.styles';
-
+import HomepageProductSegment from './../../components/homepage-product-segment//homepage-product-segment.component';
+import Footer from './../../components/footer/footer.component';
 export default function HomePage() {
   const classes = useStyles();
 
   return (
     <Container>
       <HeroBanner />
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
+      <SegmentTitle />
+      <SegmentModule />
+      <HomepageProductSegment />
+      <Footer />
     </Container>
   );
 }
