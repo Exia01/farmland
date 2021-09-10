6f315c54b9db5bfc7551660758a8e936c740e5c5
Jose <gonzalezj@protonmail.com>
2021-09-09 21:58:08 -0400

    Add onClickHandler for button

--- client/src/components/homepage-product-segment/homepage-product-segment.component.jsx
+++ client/src/components/homepage-product-segment/homepage-product-segment.component.jsx
@@ -1,3 +1,4 @@
+import { useHistory } from 'react-router-dom';
 import ThreeProductsCard from './../three-products-card/three-products-card.component';
 // Material Ui
 import { Typography, Grid, Button } from '@material-ui/core';
@@ -6,6 +7,12 @@ import useStyles from './homepage-product-segment.styles';
 
 export default function HomepageProductSegment() {
   const classes = useStyles();
+  let history = useHistory();
+
+  function onClickHandler() {
+    history.push('/products');
+  }
+
   return (
     <div className={classes.root}>
       <Grid container className={classes.gridContainer} justify='center'>
@@ -19,24 +26,18 @@ export default function HomepageProductSegment() {
           justify='center'
           className={classes.titleBtnLink}
         >
-          <Grid
-         
-            container
-            justify='center'
-            className={classes.titleHeader}
-          >
+          <Grid container justify='center' className={classes.titleHeader}>
             <Typography variant='h2' gutterBottom component='h2'>
               Our Delicious Products
             </Typography>
           </Grid>
           <Grid
-          
             container
             alignItems='center'
             justify='center'
             className={classes.btnLink}
           >
-            <Button variant='outlined' color='primary'>
+            <Button variant='outlined' color='primary' onClick={onClickHandler}>
               View All Products
             </Button>
           </Grid>
