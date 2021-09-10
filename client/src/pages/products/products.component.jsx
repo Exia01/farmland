fd5fc8a0dddaa2870629b50a6c9890d5ea7e983e
Jose <gonzalezj@protonmail.com>
2021-09-09 22:06:16 -0400

    Added Products from product context

--- client/src/pages/products/products.component.jsx
+++ client/src/pages/products/products.component.jsx
@@ -1,5 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grid, Paper, makeStyles } from '@material-ui/core';
+import React, { useState, useEffect, useContext } from 'react';
+import { ProductContext } from '../../contexts/product.context';
+
+// Material UI
+import {
+  Container,
+  Grid,
+  Paper,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+
+// Component
 import ProductCard from '../../components/product-card/product-card.component';
 
 const useStyles = makeStyles({
@@ -10,7 +21,7 @@ const useStyles = makeStyles({
 
 const Products = () => {
   const classes = useStyles();
-  const [allProducts, setAllProducts] = useState([]);
+  const { products } = useContext(ProductContext);
 
   useEffect(async () => {
     // let prod = await fetch('/api/products').then(res => res.json());
@@ -25,26 +36,27 @@ const Products = () => {
   }, []);
 
   return (
-    <Container >
-      <Grid container spacing={3} >
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} spacing={3}>
-          <ProductCard />
-        </Grid>
+    <Container>
+      <Grid container  spacing={3}  alignItems="stretch"  justify='center'>
+        {products ? (
+          products.map((individualProduct) => (
+            <Grid item xs={12} sm={6} md={4} key={individualProduct._id}>
+              <ProductCard
+                image={individualProduct.thumbnail}
+                title={individualProduct.name}
+                description={individualProduct.description}
+                productLink={individualProduct._id}
+              />
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs={10} md container justify='center'>
+            {/* if not product found */}
+            <Typography variant='h2' gutterBottom component='h2'>
+              No Products Available
+            </Typography>
+          </Grid>
+        )}
       </Grid>
     </Container>
   );
