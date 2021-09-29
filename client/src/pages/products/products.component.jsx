2b3714aec77b9305b775ff6f6024868cc41eccc7
Jose <gonzalezj@protonmail.com>
2021-09-29 15:53:20 -0400

    Add pagination
    Multiple changes.
    -Rename align to alignItems prop
    -Rename justify to justifyContent prop

--- client/src/pages/products/products.component.jsx
+++ client/src/pages/products/products.component.jsx
@@ -13,33 +13,57 @@ import {
 // Component
 import ProductCard from '../../components/product-card/product-card.component';
 
+import { Pagination } from '@material-ui/lab';
+import productService from './../../services/product-service';
+
 const useStyles = makeStyles({
   root: {
     margin: 10,
+  },
+  paginationDiv: {
+    margin: '16px 0',
   },
 });
 
 const Products = () => {
   const classes = useStyles();
-  const { products } = useContext(ProductContext);
+  // const { products } = useContext(ProductContext);
+  const [limit, setLimit] = useState(4);
+  const [skip, setSkip] = useState(1);
 
-  useEffect(async () => {
-    // let prod = await fetch('/api/products').then(res => res.json());
-    // setAllProducts(prod);
-    // -- TEMP --> Loop through all products
-    // return (allProducts.map(product => (
-    //     <Grid item xs={12} sm={6} md={4} spacing={3}>
-    //         <ProductCard product={product}/>
-    //     </Grid>
-    //     ))
-    // )
-  }, []);
+  const [currentProducts, setCurrentProducts] = useState([]);
+  const [totalDocuments, setTotalDocuments] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const onPageChangeHandler = (e, value) => {
+    setCurrentPage(value);
+    // Pemdas
+    setSkip((value - 1) * limit);
+  };
+  var totalPages = Math.ceil(totalDocuments / limit);
+  console.log(`totalPages ${totalPages}`);
+  const fetchProducts = async () => {
+    console.log(`Skip: ${skip}, Limit:${limit}`);
+    let params = { skip, limit, name: 'N-A' };
+    try {
+      let res = await productService.getAllProducts(params);
+      setTotalDocuments(res.data.total);
+      setCurrentProducts(res.data.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  useEffect(() => {
+    fetchProducts();
+    return () => {
+      // cleanup
+    };
+  }, [limit, skip, currentPage]);
   return (
     <Container>
-      <Grid container  spacing={3}  alignItems="stretch"  justify='center'>
-        {products ? (
-          products.map((individualProduct) => (
+      <Grid container spacing={3} alignItems='stretch' justifyContent='center'>
+        {currentProducts.length > 0 ? (
+          currentProducts.map((individualProduct) => (
             <Grid item xs={12} sm={6} md={4} key={individualProduct._id}>
               <ProductCard
                 image={individualProduct.thumbnail}
@@ -50,13 +74,30 @@ const Products = () => {
             </Grid>
           ))
         ) : (
-          <Grid item xs={10} md container justify='center'>
+          <Grid item xs={10} md container justifyContent='center'>
             {/* if not product found */}
             <Typography variant='h2' gutterBottom component='h2'>
               No Products Available
             </Typography>
           </Grid>
         )}
+
+        {currentProducts.length > 0 ? (
+          <Grid
+            item
+            xs={10}
+            md={12}
+            container
+            justifyContent='center'
+            className={classes.paginationDiv}
+          >
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={onPageChangeHandler}
+            />{' '}
+          </Grid>
+        ) : null}
       </Grid>
     </Container>
   );
