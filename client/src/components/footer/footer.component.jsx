d6425f49c801ea3cb7d713e9272b4b9ad083fbfc
Jose <gonzalezj@protonmail.com>
2021-10-24 16:18:00 -0400

    Correct link colors. Update container sizing

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -3,31 +3,37 @@ import { Grid, Typography } from '@material-ui/core';
 import useStyles from './footer.style';
 
 export default function Footer() {
+  const classes = useStyles();
   return (
-    <Grid container spacing={2} justifyContent='center'>
-      <Grid item xs={12} lg={3}>
+    <Grid
+      container
+      spacing={2}
+      justifyContent='center'
+      className={classes.root}
+    >
+      <Grid item xs={7} lg={4}>
         <Typography variant='body1'>
-          Credits to the following artist for the pictures:
+          Credits to the following artists for the following icons:
         </Typography>
       </Grid>
-      <Grid item xs={12} lg={12} style={{ padding: 0 }}></Grid>
-      <Grid item xs={12} lg={2}>
+      <Grid item xs={7} lg={12} style={{ padding: 0 }}></Grid>
+      <Grid item xs={7} lg={2}>
         <Typography variant='caption' display='block' gutterBottom>
-          <a href='https://icons8.com/icon/12131/family'>
+          <a href='https://icons8.com/icon/12131/family' target="_blank" rel="noopener noreferrer">
             Family icon by Icons8
           </a>
         </Typography>
       </Grid>
-      <Grid item xs={12} lg={2}>
+      <Grid item xs={7} lg={2}>
         <Typography variant='caption' display='block' gutterBottom>
-          <a href='https://icons8.com/icon/20873/organic-food'>
+          <a href='https://icons8.com/icon/20873/organic-food' target="_blank" rel="noopener noreferrer">
             Organic Food icon by Icons8
           </a>
         </Typography>
       </Grid>
-      <Grid item xs={12} lg={2}>
+      <Grid item xs={7} lg={2}>
         <Typography variant='caption' display='block' gutterBottom>
-          <a href='https://icons8.com/icon/DHWWJczuijHH/renewable-energy'>
+          <a href='https://icons8.com/icon/DHWWJczuijHH/renewable-energy' target="_blank" rel="noopener noreferrer">
             Renewable Energy icon by Icons8
           </a>
         </Typography>
