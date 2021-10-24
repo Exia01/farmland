24c9b15b695d513bd45b8d7748f786fe57a70ead
Jose <gonzalezj@protonmail.com>
2021-10-24 16:17:27 -0400

    Update bottom-navigation-footer.styles.js

--- client/src/components/bottom-navigation-footer/bottom-navigation-footer.styles.js
+++ client/src/components/bottom-navigation-footer/bottom-navigation-footer.styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'fixed',
+    position: 'relative',
     bottom: '0px',
     right: '0px',
     left: '0px',
