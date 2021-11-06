7a004609ef6588d599f737beda1170febfacba8b
Jose <gonzalezj@protonmail.com>
2021-11-06 16:55:42 -0400

    removed unused comments

--- server/utils/auth/check-jwt.js
+++ server/utils/auth/check-jwt.js
@@ -1,9 +1,8 @@
 const jwt = require('express-jwt');
+
+
 const checkJwt = jwt({
   secret: process.env.TOKEN_SECRET,
-  //here we define the token for a different 'api' from-to essentially
-  // issuer: 'api.farmland',
-  // audience: 'api.farmland',
   algorithms: ['HS256'],
   getToken: function (req) {
     return req.cookies.token;
