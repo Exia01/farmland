d07e6c21882b490a1fa7ed3ce23a4178369518c2
Jose <gonzalezj@protonmail.com>
2021-11-08 22:11:42 -0500

    update token verification middleware

--- server/utils/messages/messages.router.js
+++ server/utils/messages/messages.router.js
@@ -1,10 +1,9 @@
 /**
  * Required External Modules and Interfaces
  */
-
 const express = require('express');
+const { verifyToken } = require('../auth');
 const { getPublicMessage, getProtectedMessage } = require('./messages.service');
-const { checkJwt } = require('../auth/check-jwt');
 
 /**
  * Router Definition
@@ -23,7 +22,7 @@ messagesRouter.get('/public-message', async (req, res) => {
   res.status(200).send(message);
 });
 
-messagesRouter.get('/protected-message', checkJwt, async (req, res) => {
+messagesRouter.get('/protected-message', verifyToken, async (req, res) => {
   try {
     const message = getProtectedMessage();
     res.status(200).send(message);
