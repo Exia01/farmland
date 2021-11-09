741b1eab8871d68ecb4506b9b080d04e04f7692a
Jose <gonzalezj@protonmail.com>
2021-11-08 22:10:44 -0500

    add verifyTokenAndAuthorization middleware

--- server/routes/v1/product.route.js
+++ server/routes/v1/product.route.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 const productController = require('../../controllers/product.controller');
-const { checkJwt } = require('../../utils/auth/check-jwt');
+const { verifyTokenAndAuthorization } = require('../../utils/auth');
 
 // Routes start with /auth
 router.get('/', productController.index);
-router.post('/', productController.createProduct);
+router.post('/', verifyTokenAndAuthorization, productController.createProduct);
 router.get('/:product_id', productController.getProduct);
-router.put('/:product_id', productController.updateProduct);
-router.delete('/:product_id', productController.deleteProduct);
+router.put(
+  '/:product_id',
+  verifyTokenAndAuthorization,
+  productController.updateProduct
+);
+router.delete(
+  '/:product_id',
+  verifyTokenAndAuthorization,
+  productController.deleteProduct
+);
 
 module.exports = router;
