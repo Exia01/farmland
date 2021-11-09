67caa151deaff46ffd04665418304aa4592c0583
Jose <gonzalezj@protonmail.com>
2021-11-08 22:11:16 -0500

    rework logic for verifying token

--- server/utils/auth/index.js
+++ server/utils/auth/index.js
@@ -2,32 +2,6 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const jwtDecode = require('jwt-decode');
 
-const isAuthorized = (req, res, next) => {
-  // Capture Token
-  const cookie = req.cookies.token;
-  if (!cookie) {
-    return res.status(403).json({ msg: 'Token Missing, Authorization Denied' });
-  }
-
-  // const token = req.header('x-auth-token');
-  // if(!token) {
-  //     return res.status(403).json({ msg: "Token Missing, Authorization Denied"});
-  // }
-
-  // --> Verify Token
-  // const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-  const verified = jwt.verify(cookie, process.env.TOKEN_SECRET);
-  if (!verified) {
-    return res.status(403).json({ msg: 'Token Failed, Authorization Denied' });
-  }
-
-  // Set User ID
-  req.user = verified.id;
-  // Call Next middleware
-  console.log('Calling NEXT Middleware');
-  next();
-};
-
 const hashPassword = (password) => {
   return new Promise((resolve, reject) => {
     // Generate a salt at level 12 strength
@@ -59,7 +33,7 @@ const createToken = (user) => {
       iss: '', //issuer
       aud: '', //audience,
     },
-    process.env.TOKEN_SECRET,
+    process.env.JWT_SECRET,
     { algorithm: 'HS256', expiresIn: '1h' }
   );
 };
@@ -68,27 +42,91 @@ const verifyPassword = (passwordAttempt, hashedPassword) => {
   return bcrypt.compare(passwordAttempt, hashedPassword);
 };
 
-const attachUser = (req, res, next) => {
-  const token = req.cookies.token;
+const verifyToken = (req, res, next) => {
+  const token =
+    req.cookies.token || req.headers.token || req.header('x-auth-token');
 
   if (!token) {
     return res.status(401).json({ message: 'Authentication invalid' });
   }
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+  if (!verified) {
+    return res.status(403).json({ msg: 'Authentication invalid' });
+  }
+  req.user = verified;
+  next();
+};
 
+const verifyTokenAndAuthorization = (req, res, next) => {
+  return verifyToken(req, res, () => {
+    if (req.user._id === req.params.id || req.user.role == 'admin') {
+      next();
+    } else {
+      return res.status(403).json({ msg: 'Authentication invalid' });
+    }
+  });
+};
 
-  const decodedToken = jwtDecode(token);
+//ideally we would like to request db to confirm the privileges?
+const verifyTokenAndAdmin = (req, res, next) => {
+  return verifyToken(req, res, () => {
+    if (req.user.role == 'admin') {
+      next();
+    } else {
+      console.log('no admin rights');
+      return res.status(403).json({ msg: 'Authentication invalid' });
+    }
+  });
+};
 
-  if (!decodedToken) {
-    return res.status(401).json({
-      msg: 'There was a problem authorizing the request',
-    });
+//No longer implemented
+const isAuthorized = (req, res, next) => {
+  // Capture Token
+  const cookie = req.cookies.token;
+  if (!cookie) {
+    return res.status(403).json({ msg: 'Token Missing, Authorization Denied' });
+  }
+
+  // const token = req.header('x-auth-token');
+  // if(!token) {
+  //     return res.status(403).json({ msg: "Token Missing, Authorization Denied"});
+  // }
+
+  // --> Verify Token
+  // const verified = jwt.verify(token, process.env.JWT_SECRET);
+  const verified = jwt.verify(cookie, process.env.JWT_SECRET);
+  if (!verified) {
+    return res.status(403).json({ msg: 'Token Failed, Authorization Denied' });
+  }
+
+  // Set User ID
+  req.user = verified.id;
+  // Call Next middleware
+  console.log('Calling NEXT Middleware');
+  next();
+};
+
+//No longer used
+const verifyTokenV2 = (req, res, next) => {
+  const token = req.cookies.token || req.headers.token;
+  if (token) {
+    try {
+      const decodedToken = jwtDecode(token);
+      console.log('Decoded Token', decodedToken);
+      req.user = decodedToken;
+      next();
+    } catch (err) {
+      console.log('verifyToken Error:', err);
+      return res.status(401).json({
+        msg: 'There was a problem authorizing the request',
+      });
+    }
   } else {
-    console.log('Decoded Token', decodedToken);
-    req.user = decodedToken;
-    next();
+    return res.status(401).json({ message: 'Authentication invalid' });
   }
 };
 
+//may or may not be implemented
 const requireAdmin = (req, res, next) => {
   if (!req.user) {
     return res.status(401).json({
@@ -102,9 +140,10 @@ const requireAdmin = (req, res, next) => {
 };
 
 module.exports = {
-  isAuthorized,
   hashPassword,
   createToken,
   verifyPassword,
-  attachUser,
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 };
