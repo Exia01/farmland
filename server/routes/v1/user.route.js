3dca8d086cf0adb1980fa6407a13f7bfa4b479d7
Jose <gonzalezj@protonmail.com>
2021-11-09 21:30:38 -0500

    add user stats route. Comment suggestion on middleware

--- server/routes/v1/user.route.js
+++ server/routes/v1/user.route.js
@@ -6,8 +6,10 @@ const {
   verifyTokenAndAdmin,
 } = require('../../utils/auth');
 
+//No sure if we can apply middleware by section?
 router.get('/', userController.index);
 router.put('/:id', verifyTokenAndAuthorization, userController.updateUser);
 router.delete('/:id', verifyTokenAndAdmin, userController.deleteUser);
+router.get('/stats', verifyTokenAndAdmin, userController.userStats);
 
 module.exports = router;
