81f0bf48179abae7d3e34b8b2a2e00330ab7ea8e
Jose <gonzalezj@protonmail.com>
2021-11-09 21:29:35 -0500

    move login and create user to auth route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -13,138 +13,29 @@ const User = require('../models/user.model');
 
 // @@ Route : /users
 exports.index = async (req, res, next) => {
-  try {
-    const users = await User.find({}).lean().exec();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(400).json({ msg: 'Unable to complete request', error: err });
+  let limit = parseInt(req.query.limit); // Make sure to parse the limit to number
+  let skip = parseInt(req.query.skip); // Make sure to parse the skip to number
+  let queryObj = {};
+  if (!limit) {
+    limit = 0;
   }
-};
-
-//Move to Auth
-// @@ REGISTER ROUTE
-// @@
-exports.createUser = async (req, res, next) => {
-  try {
-    let { name, email, password, passwordCheck } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).json({ msg: 'Required field(s) missing' });
-    }
-
-    if (password.length < 5) {
-      return res
-        .status(400)
-        .json({ msg: 'Password must be at least 5 characters long' });
-    }
-
-    // Encrypt password
-    const hashedPassword = await hashPassword(password);
-
-    const userData = {
-      // implement optional chaining for name
-      email: email.toLowerCase(),
-      password: hashedPassword,
-      role: req.body.role || 'admin',
-    };
-
-    // User Email already exists in database (?)
-    const userExists = await User.findOne({ email: userData.email }).lean();
-    if (userExists) {
-      return res.status(400).json({ msg: 'User already exists!' });
-    }
-
-    const newUser = new User(userData);
-    const savedUser = await newUser.save();
-
-    if (savedUser) {
-      const token = createToken(savedUser);
-      const decodedToken = jwtDecode(token);
-      const expiresAt = decodedToken.exp;
-      // send token in HTTP-only Cookie
-
-      const { name, email, role } = savedUser;
-
-      const userInfo = {
-        name,
-        email,
-        role,
-      };
-
-      //tell express to include cookie
-      res.cookie(
-        'token',
-        token,
-        { httpOnly: true, expires: new Date(Date.now() + 8 * 3600000) } // cookie will be removed after 8 hours},
-      );
-      // Send JSON response
-      return res.status(200).json({
-        msg: 'User created!',
-        token,
-        userInfo,
-        expiresAt,
-        role,
-      });
-    } else {
-      return res.status(400).json({
-        msg: 'There was a problem creating your account',
-      });
-    }
-  } catch (err) {
-    let errMsg = err.stack;
-    let message = 'Could not complete request';
-    return res.status(500).json({ msg: message, errMsg });
+  if (!skip) {
+    skip = 0;
   }
-};
-
-//Move to Auth
-// @@ LOGIN ROUTE
-// @@
-exports.loginUser = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const [users, total] = await Promise.all([
+      User.find(queryObj)
+        .sort({ _id: -1 }) // Use this to sort documents by newest first
+        .skip(skip) // Always apply 'skip' before 'limit'
+        .limit(limit) // This is your 'page size'
+        .lean()
+        .exec(),
+      User.countDocuments(),
+    ]);
 
-    // Empty Validation
-    if (!email || !password) {
-      return res.status(400).json({ msg: 'Required field(s) missing' });
-    }
-    // Find Existing User (?)
-    const currentUser = await User.findOne({ email: email }).lean();
-
-    if (!currentUser) {
-      return res.status(403).json({ msg: 'Wrong email or password.' });
-    }
-
-    // Compare Password
-    const passwordValid = await verifyPassword(password, currentUser.password);
-    if (!passwordValid) {
-      return res.status(403).json({ msg: 'Wrong email or password.' });
-    } else {
-      const { password, name, ...rest } = currentUser;
-      //Copying onto new obj with the rest of properties
-      const userInfo = Object.assign({}, { ...rest });
-      console.log(userInfo);
-      const token = createToken(userInfo);
-
-      const decodedToken = jwtDecode(token);
-      const expiresAt = decodedToken.exp;
-
-      // send token in HTTP-only Cookie
-      res.cookie('token', token, { httpOnly: true });
-
-      // Send Successful Response
-      res.status(200).json({
-        msg: 'Authentication successful!',
-        token,
-        userInfo,
-        expiresAt,
-      });
-    }
+    res.status(200).json({ users, total });
   } catch (err) {
-    let errMsg = err.stack;
-    let message = 'Could not complete request';
-    console.log(errMsg);
-    res.status(500).json({ msg: message, errMsg });
+    res.status(500).json({ msg: 'Unable to complete request', error: err });
   }
 };
 
@@ -202,34 +93,49 @@ exports.deleteUser = async (req, res, next) => {
   }
 };
 
-// // @@ VERIFICATION ROUTE
-// // @@
-
-exports.verifyToken = async (req, res, next) => {
+//@@ GET USER STATS
+// @@ Route : /users
+exports.userStats = async (req, res, next) => {
+  const date = new Date();
+  // default last year?
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1)); // || parse date
+  let limit = parseInt(req.query.limit); // Make sure to parse the limit to number
+  let skip = parseInt(req.query.skip); // Make sure to parse the skip to number
+  let queryObj = {};
+  if (!limit) {
+    limit = 0;
+  }
+  if (!skip) {
+    skip = 0;
+  }
   try {
-    // Check Cookie for Token
-    const cookie = req.cookies.token;
-    if (!cookie) {
-      return res.json(false);
-    }
+    const [total, userAggregates] = await Promise.all([
+      User.countDocuments(),
+      User.aggregate([
+        //   //condition -> createdAt -> greater than last year
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          //month variable -> take the month date inside the createdAt
+          $project: {
+            month: { $month: '$createdAt' },
+          },
+        },
+        {
+          //group by month the item being the id
+          $group: {
+            _id: '$month',
+            total: { $sum: 1 },
+          },
+        },
+      ]),
+    ]);
+    // res.status(200).json(data);
 
-    // -- Verify Token
-    const verified = jwt.verify(cookie, process.env.JWT_SECRET);
-    // console.log('verified', verified);
-
-    if (!verified) return res.json(false);
-
-    // -- Valid User (?)
-    const user = await User.findById({ _id: verified.user._id }).lean().exec();
-
-    // console.log(user);
-    if (!user) return res.json(false);
-
-    // -- Return TRUE if valid token || to WHERE ??
-    return res.json(true);
+    res.status(200).json({ users: userAggregates, totalUsers: total });
   } catch (err) {
-    console.log(err.stack);
-    console.log(err);
-    return res.status(400).json(false);
+    let errMsg = err.stack;
+    let message = 'Could not complete request';
+    console.log(errMsg);
+    res.status(500).json({ msg: message, errMsg });
   }
 };
