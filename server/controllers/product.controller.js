6050bdee73b7fa322c264947100c80aabe8a64a5
Jose <gonzalezj@protonmail.com>
2021-11-10 14:22:26 -0500

    Update product.controller.js

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,16 +1,24 @@
 const httpStatus = require('http-status');
+const { log } = require('winston');
 const Product = require('../models/product.model');
-const ProoductVariant = require('../models/variant.model');
+const Variant = require('../models/variant.model');
 exports.index = async (req, res, next) => {
   // console.log(req.user);
   let limit = parseInt(req.query.limit); // Make sure to parse the limit to number
   let skip = parseInt(req.query.skip); // Make sure to parse the skip to number
+  let department;
+  let sortBy = { _id: -1 };
   // console.log(`limit:${limit} .... skip: ${skip}`);
   if (!limit) {
     limit = 0;
   }
   if (!skip) {
     skip = 0;
+  }
+  if (req.query.department) {
+  }
+  if (req.query.qNew) {
+    sortBy = { createAt: -1 };
   }
 
   console.log(skip, limit);
@@ -25,11 +33,11 @@ exports.index = async (req, res, next) => {
   try {
     const [products, total] = await Promise.all([
       Product.find(queryObj)
-        .sort({ _id: -1 }) // Use this to sort documents by newest first
+        .sort(sortBy) // Use this to sort documents by newest first
         .skip(skip) // Always apply 'skip' before 'limit'
         .limit(limit) // This is your 'page size'
         .lean()
-        .populate('productVariants')
+        .populate('variants')
         .exec(),
       Product.countDocuments(),
     ]);
@@ -51,16 +59,18 @@ exports.index = async (req, res, next) => {
 exports.createProduct = async (req, res, next) => {
   try {
     req.body.price = 9.99;
-    req.body.soldOut = false;
+    req.body.inStock = true;
+    req.body.addedBy = req.user.sub;
     // if we wanted to use the user to tie into the product
     // const { sub } = req.user; //from attach user middleware
     console.log(req.body);
-    const { name, description, department, price, listed, soldOut } = req.body;
-    if (!name || !description) {
+    const { name, desc, department, price, listed, inStock, ...rest } =
+      req.body;
+    if (!name || !desc) {
       return res.status(400).json({ msg: 'Required field(s) missing' });
     }
     // Check if product already exist
-    const currentProduct = await Product.findOne({ name }).exec();
+    const currentProduct = await Product.findOne({ name }).lean().exec();
     if (currentProduct)
       return res
         .status(400)
@@ -68,11 +78,12 @@ exports.createProduct = async (req, res, next) => {
 
     const newProduct = new Product({
       name,
-      description,
+      desc,
       department,
       price,
       listed,
-      soldOut,
+      inStock,
+      ...rest,
     });
 
     // Save Product to DB
@@ -91,11 +102,11 @@ exports.getProduct = async (req, res, next) => {
     const id = req.params.product_id;
     const foundProduct = await Product.findById(id)
       .lean()
-      .populate('productVariants')
+      .populate('variants')
       .exec();
     // console.log(Product.findById(id).populate('productVariants').exec());
     return res.status(200).json({ product: foundProduct });
-  } catch (error) {
+  } catch (err) {
     let errMsg = err.stack;
     let message = 'Could not complete request';
     res.status(500).json({ msg: message, errMsg });
@@ -105,10 +116,12 @@ exports.getProduct = async (req, res, next) => {
 exports.updateProduct = async (req, res, next) => {
   try {
     const id = req.params.product_id;
-    const { name, description, price, listed, soldOut } = req.body;
-    if (!name || !description || !price || !listed) {
+    const { name, desc, price, listed, inStock } = req.body;
+    if (!name || !desc || !price) {
       return res.status(400).json({ msg: 'Required field(s) missing' });
     }
+
+    //could also use "{$set:req.body} to update
     const product = await Product.findByIdAndUpdate(id, req.body, {
       new: true,
     }).exec();
