ae957a99fb78c039bc1324f5c7f254c0dcc6f5b9
Jose <gonzalezj@protonmail.com>
2021-12-06 21:15:00 -0500

    setup basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+//Components
 
+// Pages
+import Landing from './pages/Landing';
+import Navbar from './components/Navbar';
+
+// Css
+
+// MUI
+import { Container, Paper, ThemeProvider } from '@mui/material';
+
+// Context
+import { ThemeContext } from './context/Theme/index';
 export default function App() {
+  const theme = useContext(ThemeContext);
   return (
-    <div>
-      Hello World!
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={0}>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Landing />} />
+          </Routes>
+        </Paper>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
