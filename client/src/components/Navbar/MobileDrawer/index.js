89292813aacde4d3f9758482d0a1c90fe45fd4c0
Jose <gonzalezj@protonmail.com>
2021-12-06 21:16:29 -0500

    Add Navbar

---
+++ client/src/components/Navbar/MobileDrawer/index.js
@@ -0,0 +1,57 @@
+import { styled } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 250;
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function MobileDrawer({ open, handleDrawerClose }) {
+  return (
+    <Drawer
+      sx={{
+        backgroundColor:'primary.main',
+        // width: drawerWidth,
+        // flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      anchor='right'
+      onClose={handleDrawerClose}
+      open={open}
+    >
+      {/* <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </DrawerHeader> */}
+      {/* <Divider /> */}
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
+}
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,137 @@
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import ThemeToggle from '../ThemeToggle';
+
+// Logo
+import logo from '../../uploads/images/icons/farmlandLogo3.png';
+
+// MUI
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Box,
+  Typography,
+  Container,
+  Link,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import MobileDrawer from './MobileDrawer/index';
+
+// styles
+// A way to do do conditionals
+// sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose(e) {
+    if (e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift')) {
+      return;
+    }
+    setOpen(false);
+  }
+
+  return (
+    <>
+      <AppBar
+        title='Farmland'
+        elevation={0}
+        position='relative'
+        sx={{ padding: '16px 0' }}
+      >
+        <Container disableGutters={true}>
+          <Toolbar>
+            {/* Left Section */}
+            <div>
+              <Link component={RouterLink} to='/'>
+                <Box
+                  component='img'
+                  sx={{
+                    maxHeight: { xs: 70 },
+                    maxWidth: { xs: '100%', md: 189 },
+                  }}
+                  alt='Logo'
+                  src={logo}
+                />
+              </Link>
+            </div>
+
+            {/* center div */}
+            <Container>
+              <Box
+                component='div'
+                sx={{
+                  display: { xs: 'none', md: 'flex' },
+                  justifyContent: 'center',
+                  gap: 2,
+                }}
+              >
+                <Link
+                  component={RouterLink}
+                  to='/'
+                  sx={{ color: 'text.primary' }}
+                  underline='hover'
+                >
+                  <Typography variant='h6' gutterBottom component='div'>
+                    Buy Online
+                  </Typography>
+                </Link>
+                <Link
+                  component={RouterLink}
+                  to='/'
+                  sx={{ color: 'text.primary' }}
+                  underline='hover'
+                >
+                  <Typography variant='h6' gutterBottom component='div'>
+                    About Us{' '}
+                  </Typography>
+                </Link>
+              </Box>
+            </Container>
+
+            {/* Right Section */}
+            <Box
+              component='div'
+              sx={{
+                flex: '0 0 20%',
+                display: 'flex',
+                gap: 0.5,
+                justifyContent: 'flex-end',
+              }}
+            >
+              <ThemeToggle />
+              <IconButton color='inherit'>
+                <ShoppingCart />
+              </IconButton>
+              <IconButton
+                color='inherit'
+                onClick={handleDrawerOpen}
+                sx={{ display: { md: 'none' } }}
+              >
+                
+                <MenuIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <MobileDrawer
+        handleDrawerClose={handleDrawerClose}
+        open={open}
+      ></MobileDrawer>
+    </>
+  );
+}
+
+// );
+
+// https://stackoverflow.com/questions/68471759/when-migrating-to-material-ui-v5-how-to-deal-with-conditional-classes/68472931#68472931
+
+// https://stackoverflow.com/questions/69500357/how-to-implement-conditional-styles-in-mui-v5-sx-prop
