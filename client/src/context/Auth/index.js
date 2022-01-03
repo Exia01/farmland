15358544602355e32d6932fbb63ce85420a7f810
Jose <gonzalezj@protonmail.com>
2022-01-03 16:50:24 -0500

    Update relative import

--- client/src/context/Auth/index.js
+++ client/src/context/Auth/index.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react';
-import { userReducer } from '../reducer/userReducer';
+import { userReducer } from './../../reducer/Auth';
 
 const AuthContext = createContext();
 const AuthDispatchContext = createContext();
