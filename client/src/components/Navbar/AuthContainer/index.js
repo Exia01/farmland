0ce9d61b00dd9852b9412eee2373edb84285b5af
Jose <gonzalezj@protonmail.com>
2022-01-03 16:41:24 -0500

    Update AuthContainer to include links and dynamic rendering

---
+++ client/src/components/Navbar/AuthContainer/index.js
@@ -0,0 +1,96 @@
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../../context/Auth';
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
+
+// MUI
+import {
+  Typography,
+  Link,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+
+export default function AuthContainer() {
+  let navigate = useNavigate();
+  const { isAuthenticated } = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  let isAuth = isAuthenticated();
+
+  function handleMenuClick(e) {
+    setAnchorEl(e.currentTarget);
+  }
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  function onButtonClickHandler() {
+    navigate('./login');
+  }
+
+  function onIconClickHandler() {}
+
+  if (!isAuthenticated()) {
+    return (
+      <Button
+        variant='text'
+        sx={{ color: 'text.primary' }}
+        onClick={onButtonClickHandler}
+      >
+        Login
+      </Button>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <IconButton
+          size='large'
+          aria-label={isAuth ? 'account of current user menu' : 'undefined'}
+          aria-controls='account-menu-appbar'
+          aria-haspopup='true'
+          onClick={handleMenuClick}
+          sx={{ color: 'text.primary' }}
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id='basic-menu'
+          anchorEl={anchorEl}
+          open={isMenuOpen}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          // Below disables adding padding
+          disableScrollLock={true}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link
+              component={RouterLink}
+              underline='none'
+              color={'inherit'}
+              to='/account'
+            >
+              My Account
+            </Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </div>
+    </>
+  );
+}
